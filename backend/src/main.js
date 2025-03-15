de9ff89922324a5df501392a1a81919beb7a0e68
Masudur Rabbi Rahat <rabbirahat9@gmail.com>
2025-03-15 13:30:56 +0600

    Initial commit

---
+++ backend/src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import store from './store'
+import router from './router'
+import './index.css'
+import App from './App.vue'
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
---
+++ backend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+        require('@tailwindcss/forms'),
+  ],
+}
